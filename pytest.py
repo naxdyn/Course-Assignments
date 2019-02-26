f3c1d3635850485030f4b722d1522a23bc6550c3
unknown <danny.wang@ryerson.ca>
2019-02-26 12:51:59 -0500

    fixed file names

--- pytest.py
+++ pytest.py
@@ -1,11 +1,7 @@
 import unittest
-
-import my_functions
+import functions
 
 class TestMyFunc(unittest.TestCase):
-
-    def setUp(self):
-        pass
 
     def test_increment_one_1(self):
         self.assertEqual( my_functions.increment_by_one(1), 2)
