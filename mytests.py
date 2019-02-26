ba2a13e1581eaf4bc85e5701ee05dc251e1a5111
unknown <danny.wang@ryerson.ca>
2019-02-26 13:36:48 -0500

    fixed test indentation

--- mytests.py
+++ mytests.py
@@ -9,13 +9,13 @@ class TestMyFunc(unittest.TestCase):
     def test_double1(self):
         self.assertEqual(myfunctions.double(1), 2)
 		
-	def test_double2(self):
+    def test_double2(self):
         self.assertEqual(myfunctions.double(3), 6)
 		
-	def test_halve1(self):
+    def test_halve1(self):
         self.assertEqual(myfunctions.halve(0), 0)
 		
-	def test_halve2(self):
+    def test_halve2(self):
         self.assertEqual(myfunctions.halve(4), 2)
 
 if __name__ == '__main__':
