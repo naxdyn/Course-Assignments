626a303459ce5001b32d9dc04678666e463146dd
unknown <danny.wang@ryerson.ca>
2019-02-26 13:04:29 -0500

    Created new functions and tests

--- pytest.py
+++ pytest.py
@@ -1,19 +1,22 @@
 import unittest
-import functions
+import myfunctions
 
 class TestMyFunc(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_increment_one_1(self):
-        self.assertEqual(functions.increment_by_one(1), 2)
-
-    def test_increment_one_2(self):
-        self.assertEqual(functions.increment_by_one(0), 1)
-
-    def test_increment_two(self):
-        self.assertEqual(functions.increment_by_two(0), 2)
+    def test_double1(self):
+        self.assertEqual(myfunctions.double(1), 2)
+		
+	def test_double2(self):
+        self.assertEqual(myfunctions.double(3), 6)
+		
+	def test_halve1(self):
+        self.assertEqual(myfunctions.halve(0), 0)
+		
+	def test_halve2(self):
+        self.assertEqual(myfunctions.halve(4), 2)
 
 if __name__ == '__main__':
 unittest.main()
