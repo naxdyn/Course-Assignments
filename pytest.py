e8d668175cb5f59a08b8ce7783b52d5283ec8db4
unknown <danny.wang@ryerson.ca>
2019-02-26 12:54:48 -0500

    fixed pytest

--- pytest.py
+++ pytest.py
@@ -3,6 +3,9 @@ import functions
 
 class TestMyFunc(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     def test_increment_one_1(self):
         self.assertEqual( my_functions.increment_by_one(1), 2)
 
