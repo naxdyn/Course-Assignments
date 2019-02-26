65dce67123aa4cc7506fccd3bcd623f920c53285
unknown <danny.wang@ryerson.ca>
2019-02-26 12:57:13 -0500

    fixed pytest again

--- pytest.py
+++ pytest.py
@@ -7,13 +7,13 @@ class TestMyFunc(unittest.TestCase):
         pass
 
     def test_increment_one_1(self):
-        self.assertEqual( my_functions.increment_by_one(1), 2)
+        self.assertEqual(functions.increment_by_one(1), 2)
 
     def test_increment_one_2(self):
-        self.assertEqual( my_functions.increment_by_one(0), 1)
+        self.assertEqual(functions.increment_by_one(0), 1)
 
     def test_increment_two(self):
-        self.assertEqual( my_functions.increment_by_two(0), 2)
+        self.assertEqual(functions.increment_by_two(0), 2)
 
 if __name__ == '__main__':
 unittest.main()
